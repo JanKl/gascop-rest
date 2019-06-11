02b427eeac954b5f5350538fa3dec6da475b64a2
Jan <JanKl@users.noreply.github.com>
2019-06-11 11:02:58 +0200

    Include map files in the output for the GUI
    Fixes #9

--- build.js
+++ build.js
@@ -12,9 +12,13 @@ shell.cp("-r", "src_js/*", "build");
 
 // Copy JavaScript GUI frameworks
 shell.cp("node_modules/jquery/dist/jquery.min.js", "build/public/javascripts/jquery.min.js");
+shell.cp("node_modules/jquery/dist/jquery.min.map", "build/public/javascripts/jquery.min.map");
 shell.cp("node_modules/popper.js/dist/umd/popper.min.js", "build/public/javascripts/popper.min.js");
+shell.cp("node_modules/popper.js/dist/umd/popper.min.js.map", "build/public/javascripts/popper.min.js.map");
 shell.cp("node_modules/bootstrap/dist/js/bootstrap.min.js", "build/public/javascripts/bootstrap.min.js");
+shell.cp("node_modules/bootstrap/dist/js/bootstrap.min.js.map", "build/public/javascripts/bootstrap.min.js.map");
 shell.cp("node_modules/bootstrap/dist/css/bootstrap.min.css", "build/public/stylesheets/bootstrap.min.css");
+shell.cp("node_modules/bootstrap/dist/css/bootstrap.min.css.map", "build/public/stylesheets/bootstrap.min.css.map");
 
 // Transpile TypeScript
 shell.exec("npm run tsc");
