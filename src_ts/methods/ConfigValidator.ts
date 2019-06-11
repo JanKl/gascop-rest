bfc96b9d2f0543a1195435f84f04aa0beacfe0a2
Jan <JanKl@users.noreply.github.com>
2019-06-11 14:47:04 +0200

    Implemented config check of predefined pager groups

--- src_ts/methods/ConfigValidator.ts
+++ src_ts/methods/ConfigValidator.ts
@@ -1,6 +1,8 @@
 import { ConfigObj } from "../interfaces/ConfigObj";
 import fs from "fs";
 import path from "path";
+import { ConfigPredefinedPagerGroup } from "../interfaces/ConfigPredefinedPagerGroup";
+import { ConfigPredefinedPager } from "../interfaces/ConfigPredefinedPager";
 
 var config: ConfigObj = require('../../config.json');
 
@@ -11,6 +13,10 @@ export class ConfigValidator {
         }
 
         if (!this.ArePredefinedMessagesValid()) {
+            return false;
+        }
+
+        if (!this.ArePredefinedPagerGroupsValid()) {
             return false;
         }
 
@@ -59,6 +65,118 @@ export class ConfigValidator {
                 return false;
             }
         }
+
+        return true;
+    }
+
+    private static ArePredefinedPagerGroupsValid(): boolean {
+        if (typeof config.predefinedPagers !== "object") {
+            console.error("The predefined pagers groups are not set. Please use at least an empty array.");
+            return false;
+        }
+
+        if (config.predefinedPagers.length == 0) {
+            return true;
+        }
+
+        for (let groupLineId = 0; groupLineId < config.predefinedPagers.length; ++groupLineId) {
+            if (typeof config.predefinedPagers[groupLineId] !== "object") {
+                console.error("The predefined pager group at index " + groupLineId + " is not a valid object.");
+                return false;
+            }
+
+            if (!this.IsPredefinedPagersGroupValid(config.predefinedPagers[groupLineId], groupLineId)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static IsPredefinedPagersGroupValid(predefinedPagerGroup: ConfigPredefinedPagerGroup, groupLineId: number): boolean {
+        if (typeof predefinedPagerGroup.groupName !== "string") {
+            console.error("The name of the predefined pager group at index " + groupLineId + " is not a valid string.");
+            return false;
+        }
+
+        if (typeof predefinedPagerGroup.pagers !== "object") {
+            console.error("The predefined pager group at index " + groupLineId + " has no pagers set. Please use at least an empty array.");
+            return false;
+        }
+
+        if (predefinedPagerGroup.pagers.length == 0) {
+            return true;
+        }
+
+        for (let pagerLineId = 0; pagerLineId < predefinedPagerGroup.pagers.length; ++pagerLineId) {
+            if (typeof predefinedPagerGroup.pagers[pagerLineId] !== "object") {
+                console.error("The predefined pager at index " + pagerLineId + " of group index " + groupLineId + " is not a valid object.");
+                return false;
+            }
+
+            if (!this.IsPredefinedPagerValid(predefinedPagerGroup.pagers[pagerLineId], pagerLineId, groupLineId)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static IsPredefinedPagerValid(predefinedPager: ConfigPredefinedPager, pagerLineId: number, groupLineId: number): boolean {
+        if (typeof predefinedPager.baudRate !== "number") {
+            console.error("The information 'baudRate' of predefined pager at index " + pagerLineId + " of group index " + groupLineId + " is not a valid number.");
+            return false;
+        }
+
+        if (predefinedPager.baudRate != 512 && predefinedPager.baudRate != 1200 && predefinedPager.baudRate != 2400) {
+            console.error("The information 'baudRate' of predefined pager at index " + pagerLineId + " of group index " + groupLineId + " must be one of 512, 1200, 2400.");
+            return false;
+        }
+
+
+        if (typeof predefinedPager.functionBits !== "number") {
+            console.error("The information 'functionBits' of predefined pager at index " + pagerLineId + " of group index " + groupLineId + " is not a valid number.");
+            return false;
+        }
+
+        if (predefinedPager.functionBits < 0 || predefinedPager.functionBits > 3) {
+            console.error("The information 'functionBits' of predefined pager at index " + pagerLineId + " of group index " + groupLineId + " must be between 0 and 3.");
+            return false;
+        }
+
+
+        if (typeof predefinedPager.guiName !== "string") {
+            console.error("The information 'guiName' of predefined pager at index " + pagerLineId + " of group index " + groupLineId + " is not a valid string.");
+            return false;
+        }
+
+        if (predefinedPager.guiName == "") {
+            console.error("The information 'guiName' of predefined pager at index " + pagerLineId + " of group index " + groupLineId + " must be non-empty.");
+            return false;
+        }
+
+
+        if (typeof predefinedPager.ric !== "number") {
+            console.error("The information 'ric' of predefined pager at index " + pagerLineId + " of group index " + groupLineId + " is not a valid number.");
+            return false;
+        }
+
+        if (predefinedPager.ric < 1 || predefinedPager.ric > 2097152) {
+            console.error("The information 'ric' of predefined pager at index " + pagerLineId + " of group index " + groupLineId + " must be between 1 and 2097152.");
+            return false;
+        }
+
+
+        if (typeof predefinedPager.txChannel !== "number") {
+            console.error("The information 'txChannel' of predefined pager at index " + pagerLineId + " of group index " + groupLineId + " is not a valid number.");
+            return false;
+        }
+
+        if (predefinedPager.txChannel < 1 || predefinedPager.txChannel > 8) {
+            console.error("The information 'txChannel' of predefined pager at index " + pagerLineId + " of group index " + groupLineId + " must be between 1 and 8.");
+            return false;
+        }
+
 
         return true;
     }
